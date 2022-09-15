2a7227bab4d4ff02d7214f128d291c25a69e61f5
Ilya <iltihomsorok@gmail.com>
2022-09-16 01:30:59 +0300

    added local functionns, checked and unchecked

--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace Lab1
 {
@@ -61,6 +62,162 @@ namespace Lab1
             int? var2 = null;
             ////=======================f============================
             int var3 = 15;
+            ////strings:a
+
+            string str1 = "hto ya";
+            string str2 = "dadaya";
+            Console.WriteLine("Strings comparison:" + str1 == str2);
+            //stings:b
+            string str3 = "amogus";
+            str1 += str2;
+            Console.WriteLine("Strings concatenation: " + str1 + str3);
+            string str4 = String.Copy(str1);
+            Console.WriteLine("\nCopying: " + str4);
+            Console.WriteLine("\nSubstring: " + str2.Substring(0, 5));
+            Console.WriteLine("\nSplit: ");
+            string[] words = str1.Split(" ");
+            foreach (string word in words)
+            {
+                Console.WriteLine(word);
+            }
+            Console.WriteLine("\nInsert: " + str1.Insert(str2.Length, str3));
+            Console.WriteLine("\nRemove:" + str1.Remove(11));
+            Console.WriteLine("Interpolarion:");
+            Console.WriteLine($"First string is :{str1}");
+            //stings:c
+            string emptyString = "";
+            string nullString = null;
+            Console.WriteLine("\nempty sting:" + String.IsNullOrEmpty(emptyString) + "\nnull string: " + String.IsNullOrEmpty(nullString));
+            Console.WriteLine("Empty and null string concatenation: " + String.Concat(emptyString, str2));
+            Console.WriteLine("Empry and null string comparison: " + nullString == str1);
+            //stings:d
+            StringBuilder sb = new StringBuilder("bruv");
+            sb.Remove(2, 1);
+            sb.Append("pog");
+            sb.Insert(0, "kavo");
+            Console.WriteLine("\nStringBuilder:" + sb);
+            //arrays:a
+            int[,] matrix = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+            Console.WriteLine("===================matrix=================");
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine();
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write("\t" + matrix[i, j]);
+                }
+            }
+            //arrays:b
+            string[] strArray = { "\nsting1", "string2", "string3", "string4" };
+            Console.WriteLine("\n\nstrArray length: " + strArray.Length);
+            foreach (string s1 in strArray)
+            {
+                Console.Write(' ' + s1);
+            }
+            Console.WriteLine("\nEnter number and value of element you want to change: ");
+            Console.WriteLine("number: ");
+            int pos = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("value: ");
+            string value = Console.ReadLine();
+            strArray[pos - 1] = value;
+            Console.WriteLine("\nResult: ");
+            foreach (string s1 in strArray)
+            {
+                Console.Write(' ' + s1);
+            }
+            //arrays:c
+            Console.WriteLine("\n================Jaggged array============");
+            int[][] jaggedArr = new int[3][];
+            jaggedArr[0] = new int[2];
+            jaggedArr[1] = new int[3];
+            jaggedArr[2] = new int[4];
+            Random random = new Random();
+            for (int i = 0; i < jaggedArr.Length; i++)
+            {
+                for (int j = 0; j < jaggedArr[i].Length; j++)
+                {
+                    jaggedArr[i][j] = random.Next(9);
+                }
+            }
+            for (int i = 0; i < jaggedArr.Length; i++)
+            {
+                Console.WriteLine();
+                for (int j = 0; j < jaggedArr[i].Length; j++)
+                {
+                    Console.Write(' ');
+                    Console.Write(jaggedArr[i][j] + "\t");
+                }
+                //arrays:d
+                int[] arr = new int[3];
+                string[] strng = new string[4];
+            }
+            //corteges a)
+            ValueTuple<int, string, char, string, ulong> tuple = (228, "dabdaya", 'a', "hto ya", 70);
+            ValueTuple<int, string, char, string, ulong> newTuple = (1337, "hto ya", 'b', "dabdaya", 90);
+
+            //corteges b)
+            Console.WriteLine("\n Tupble items\n" + tuple.Item1);
+            Console.Write("\t" + tuple.Item2);
+            Console.Write("\t" + tuple.Item3);
+            Console.Write("\t" + tuple.Item4);
+            Console.Write("\t" + tuple.Item5);
+            //corteges c)
+            (int, string) tple = (1, "abc");
+            object box = tple;
+            (int, string) t = (ValueTuple<int, string>)box;
+            int[] x = { 1, 2, 3 };
+            string str7 = "string";
+            (_, _, _, str7) = GetMaxAndMin(x, str7);
+            //corteges d)
+            bool result = tuple == newTuple;
+            Console.WriteLine("\nTuples comparison: " + result);
+
+            //functions
+            //Console.WriteLine("Check max int: " + checkMaxInt());
+            Console.WriteLine("Uncheck max int: " + uncheckMaxInt());
+            (int, int, int, string) GetMaxAndMin(int[] x, string s)
+            {
+                int max = 0;
+                foreach (int elem in x)
+                {
+                    if (elem > max)
+                    {
+                        max = elem;
+                    }
+                }
+                int min = 0;
+                foreach (int elem in x)
+                {
+                    if (elem < min)
+                    {
+                        min = elem;
+                    }
+                }
+                int sum = 0;
+                foreach (int elem in x)
+                {
+                    sum += elem;
+                }
+                string firstLetter = s.Substring(0);
+                return (max, min, sum, firstLetter);
+            }
+
+            int checkMaxInt()
+            {
+                checked
+                {
+                    int mx = Int32.MaxValue;
+                    return mx + 1;
+                }
+            }
+            int uncheckMaxInt()
+            {
+                unchecked
+                {
+                    int mx = Int32.MaxValue;
+                    return mx + 1;
+                }
+            }
         }
     }
 }
